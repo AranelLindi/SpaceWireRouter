4d60c3dbdaec8e0f1fa1356897d0a6f82c896419
Stefan Lindörfer <stefan.lindoerfer@uni-wuerzburg.de>
2023-09-15 10:10:28 +0200

    Preliminary final version

--- blockdesigns/main_design/sim/main_design.vhd
+++ blockdesigns/main_design/sim/main_design.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
---Date        : Fri Aug 18 15:28:46 2023
+--Date        : Tue Sep 12 12:45:44 2023
 --Host        : stl56jc-MS-7C95 running 64-bit Ubuntu 22.04.3 LTS
 --Command     : generate_target main_design.bd
 --Design      : main_design
