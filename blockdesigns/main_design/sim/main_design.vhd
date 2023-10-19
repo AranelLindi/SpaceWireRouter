1200b586df7492abcbc1bbf1771c826ef354550a
Stefan Lindörfer <stefan.lindoerfer@uni-wuerzburg.de>
2023-10-19 16:08:27 +0200

    Hardware tests including CPU-Interface and UART-SpaceWire-Adapter were sucessful. Ready for Time-Code Measurements!

--- blockdesigns/main_design/sim/main_design.vhd
+++ blockdesigns/main_design/sim/main_design.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
---Date        : Thu Oct 19 14:39:37 2023
+--Date        : Thu Oct 19 15:41:38 2023
 --Host        : stl56jc-MS-7C95 running 64-bit Ubuntu 22.04.3 LTS
 --Command     : generate_target main_design.bd
 --Design      : main_design
