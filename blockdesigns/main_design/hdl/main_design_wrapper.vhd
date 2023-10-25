c0838539f0184e2b6bed0a69532afd603fd20d29
Stefan Lindörfer <stefan.lindoerfer@uni-wuerzburg.de>
2023-10-25 16:42:39 +0200

    Only made change: - Added CPU-Interface Rev. 71 (Edge detection for tc_in signal)

--- blockdesigns/main_design/hdl/main_design_wrapper.vhd
+++ blockdesigns/main_design/hdl/main_design_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
---Date        : Thu Oct 19 15:41:38 2023
+--Date        : Wed Oct 25 16:25:32 2023
 --Host        : stl56jc-MS-7C95 running 64-bit Ubuntu 22.04.3 LTS
 --Command     : generate_target main_design_wrapper.bd
 --Design      : main_design_wrapper
